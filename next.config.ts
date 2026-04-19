daa40b048d557bf928a9919d9c40be2081ffba2f
KARIM <Karim.sadki@gmail.com>
2026-04-19 22:30:19 +0300

    Simplify next.config.ts by removing intl plugin

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+const nextConfig = {
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
+};
 
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
-
-const nextConfig: NextConfig = {};
-
-export default withNextIntl(nextConfig);
+export default nextConfig;
